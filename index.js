76702124bbae5da6f3457d2a691a2e9ac26be362
Yytsi <tuukka.yildirim@gmail.com>
2024-04-11 16:55:49 +0300

    8.5

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -125,7 +125,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) =>
-      books.filter((book) => book.author === args.author),
+      books.filter(
+        (book) =>
+          (args.author ? book.author === args.author : true) &&
+          (args.genre ? book.genres.includes(args.genre) : true)
+      ),
     allAuthors: () =>
       authors.map((author) => ({
         ...author,
