833cac1b3aa1fa7400a5ad49175e5d4ae7923038
Yytsi <tuukka.yildirim@gmail.com>
2024-04-11 12:50:02 +0300

    8.3

--- index.js
+++ index.js
@@ -102,6 +102,13 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Author {
+    name: String!
+    bookCount: Int!
+    id: ID!
   }
 
   type Book {
@@ -118,6 +125,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () =>
+      authors.map((author) => ({
+        ...author,
+        bookCount: books.filter((book) => book.author === author.name).length,
+      })),
   },
 };
 
