6234364447b9f99a7afeee2d67574bda75055633
Yytsi <tuukka.yildirim@gmail.com>
2024-04-11 12:42:25 +0300

    8.2

--- index.js
+++ index.js
@@ -101,6 +101,15 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
   }
 `;
 
@@ -108,6 +117,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
